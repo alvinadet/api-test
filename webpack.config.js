10b2e2f5b2eed80808fcc2d6ee4be949ae590679
Arif Widianto <arifjozz123@gmail.com>
2020-10-10 22:26:56 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+console.log("Awikwok")
+
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
